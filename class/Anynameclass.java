f075e1fb12253989eeb4c49d76dc0b4bc7664e10
Tejas <tejaskait@gmail.com>
2025-08-30 04:06:17 +0530

    learnt method

---
+++ class/Anynameclass.java
@@ -0,0 +1,5 @@
+class Anynameclass{
+
+}
+
+//there can be class with anyname
---
+++ method/Mainmethod.java
@@ -0,0 +1,8 @@
+class Mainmethod{
+    public static void main(String[] args){
+
+    }
+}
+
+
+//we need atleast one main method 
