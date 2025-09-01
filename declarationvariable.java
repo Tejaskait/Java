32ab25840159eb560fbe413005ad8d7ec51147ad
Tejas <tejaskait@gmail.com>
2025-09-01 23:35:45 +0530

    variable declaration

---
+++ declarationvariable.java
@@ -0,0 +1,11 @@
+class declarationvariable{
+    public static void main(String[] args){
+        int j=5;
+        int kj=2,sd=8,ad=9;
+        int g,h;
+        int s=g=h=5;
+        int y,u=07;
+        System.out.print(h);
+
+    }
+}
