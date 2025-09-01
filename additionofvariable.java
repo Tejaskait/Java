76afc172ab6af0f3a7501f5f3530673dfd7d7b67
Tejas <tejaskait@gmail.com>
2025-09-01 23:29:05 +0530

    addition string

--- additionofvariable.java
+++ additionofvariable.java
@@ -2,11 +2,11 @@ class additionofvariable{
     public static void main(String[] args){
         boolean J = true;
         boolean K = false;
-        System.out.print(J);
+        System.out.print(J+""+K);
 
         String n="mino";
         String l="shb";
-        System.out.print(n+l);
+        System.out.print(n+" "+l);
 
 
     }
