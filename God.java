d83ff8cab6dda4a9afd1e44a7b65b9d61d71ae42
Tejas <tejaskait@gmail.com>
2025-09-17 21:59:35 +0530

    learnt primitive data types

--- God.java
+++ God.java
@@ -12,16 +12,22 @@ class God{
            int a=8;
            String stri = "meno";
            char ch='a';
+           double k = 22/7;
+           long o = 9;
+           short y = 7;
            final boolean bool=true;
                //bool=false;
            float flo=69.69f;
+     byte j=127;
            System.out.println(flo);
            System.out.println(stri);
            System.out.println(ch);
            System.out.println(bool);
            System.out.println(a);
-
-           
+System.out.println(k);
+      System.out.println(o);
+        System.out.println(y);
+           System.out.println(j);
 
     }
 }
